8ca73c5bf800fc566a6a24384f238728eb06cb54
Mauro <maurodelle7@gmail.com>
2023-08-29 13:27:27 -0300

    Actualizando

--- CLASE-01/AppN1.php
+++ CLASE-01/AppN1.php
@@ -1,10 +1,10 @@
+<?php   
 /*
     Aplicación No 1 (Sumar números)
     Confeccionar un programa que sume todos los números enteros desde 1 mientras la suma no
     supere a 1000. Mostrar los números sumados y al finalizar el proceso indicar cuantos números
     se sumaron.
 */
-<?php   
 
     $suma = 1;
     $numerosSumados = 0;
--- CLASE-01/AppN3.php
+++ CLASE-01/AppN3.php
@@ -1,3 +1,5 @@
+
+<?php
 /*
 Aplicación No 3 (Obtener el valor del medio)
 Dadas tres variables numéricas de tipo entero $a, $b y $c realizar una aplicación que muestre
@@ -6,8 +8,6 @@ variables. De no existir dicho valor, mostrar un mensaje que indique lo sucedido
 = 6; $b = 9; $c = 8; => se muestra 8.
 Ejemplo 2: $a = 5; $b = 1; $c = 5; => se muestra un mensaje “No hay valor del medio”
 */
-
-<?php
 
 $a = 6;
 $b = 9;
