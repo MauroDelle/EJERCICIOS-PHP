02bd6a0da3b48beef687cc354c6643aa2c50f354
Mauro <maurodelle7@gmail.com>
2023-08-29 13:25:44 -0300

    Actualizando repo

--- CLASE-01/AppN2.php
+++ CLASE-01/AppN2.php
@@ -1,3 +1,9 @@
+/*
+    Aplicación No 2 (Mostrar fecha y estación)
+    Obtenga la fecha actual del servidor (función date) y luego imprímala dentro de la página con
+    distintos formatos (seleccione los formatos que más le guste). Además indicar que estación del
+    año es. Utilizar una estructura selectiva múltiple.
+*/
 <?php
 
     $fechaActual = date("Y-m-d"); // Obtengo la fecha actual.
