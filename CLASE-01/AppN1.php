088adcac18e34af84016a0e3d441d16beae13254
Mauro <maurodelle7@gmail.com>
2023-08-29 13:23:44 -0300

    Actualizando Repo

--- CLASE-01/AppN1.php
+++ CLASE-01/AppN1.php
@@ -1,3 +1,9 @@
+/*
+    Aplicación No 1 (Sumar números)
+    Confeccionar un programa que sume todos los números enteros desde 1 mientras la suma no
+    supere a 1000. Mostrar los números sumados y al finalizar el proceso indicar cuantos números
+    se sumaron.
+*/
 <?php   
 
     $suma = 1;
